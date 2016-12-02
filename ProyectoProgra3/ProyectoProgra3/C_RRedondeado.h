bc0c30e51bdfb5882887e344aad5d6de94951f6f
Arturo9722 <idv15c.agonzalez@uartesdigitales.edu.mx>
2016-12-02 09:09:49 -0600

    RRedondeado Listo

--- ProyectoProgra3/ProyectoProgra3/C_RRedondeado.h
+++ ProyectoProgra3/ProyectoProgra3/C_RRedondeado.h
@@ -4,13 +4,23 @@
 
 class C_RRedondeado : public C_Rectangulo
 {
+public: //private
+	float _base, _altura;
+	float radius;
+	sf::ConvexShape _shape;
+	std::vector<sf::Vector2f> _originalpos;
 public:
 	//Guardar Cargar
 	int GetCLSID();
 	void Guardar(ofstream &out);
 	void Cargar(ifstream &in);
-	//Guardar Cargar
+	void Inicializar();
+
+	C_RRedondeado(float base, float altura);
+	void setColorRelleno(sf::Color color);
+	void setColorLinea(sf::Color color);
+	bool setPosicion(sf::Vector2f vector);
+	bool HitTest(sf::Vector2i point);
 	C_RRedondeado();
 	~C_RRedondeado();
 };
-
