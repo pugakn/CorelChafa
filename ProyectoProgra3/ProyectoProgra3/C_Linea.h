6dfa52fc8902f8528004bc16449d63276ffd40a5
pugakn <puga_deathnote@hotmail.com>
2016-12-04 14:08:37 -0600

    fix

--- ProyectoProgra3/ProyectoProgra3/C_Linea.h
+++ ProyectoProgra3/ProyectoProgra3/C_Linea.h
@@ -19,11 +19,6 @@ public:
 	void setColorLinea(sf::Color color) override;
 	bool setPosicion(sf::Vector2f vector) override;
 	bool HitTest(sf::Vector2i point) override;
-
-	void setColorRelleno(sf::Color color);
-	void setColorLinea(sf::Color color);
-	bool setPosicion(sf::Vector2f vector);
-	bool HitTest(sf::Vector2i point);
 	void Dibujar(sf::RenderWindow& window);
 	C_Linea(sf::Vector2f a, sf::Vector2f b);
 	C_Linea();
